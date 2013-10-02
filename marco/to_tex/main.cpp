94612b35d1479380a24c7c072cdbb6d14aa67c7e
Marco Torres <marco.torres.810@gmail.com>
2013-10-01 23:11:17 -0600

    to_tex module added, this creates the .tex file from a given string

---
+++ marco/to_tex/main.cpp
@@ -0,0 +1,11 @@
+#include "to_tex.h"
+#include <string>
+using namespace std;
+int main()
+{
+cout<<"Prueba de to_tex"<<endl;
+//Aqui es donde va el getCadena()
+string cadena="Hola Mundo!!!";
+	create_tex(cadena);
+cout<<"Fin"<<endl;
+}
